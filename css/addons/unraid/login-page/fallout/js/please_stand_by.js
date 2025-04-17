efd25e35f50156fb765fc5301ff3350b0d515cbb
KaHooli <james@cavanaghs.id.au>
2025-04-18 08:26:44 +1000

    Fixed broken video URL to preferred domain
    Replaced "https://gilbn.github.io/theme.park/" in URL for video file, which is broken, to @GilbN preferred domain of "https://theme-park.dev/"
    
    Co-Authored-By: GilbN <24592972+GilbN@users.noreply.github.com>

--- css/addons/unraid/login-page/fallout/js/please_stand_by.js
+++ css/addons/unraid/login-page/fallout/js/please_stand_by.js
@@ -2,6 +2,6 @@
 // Source: https://www.youtube.com/watch?v=ehvduomE0AU
 
 let video = `
-<div class="background-wrap"><video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> <source src="https:\/\/themepark-dev.github.io\/theme.park\/css\/addons\/unraid\/login-page\/fallout\/video\/please_stand_by.mp4" type="video\/mp4">Video not supported</video></div>
+<div class="background-wrap"><video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> <source src="https:\/\/theme-park.dev\/css\/addons\/unraid\/login-page\/fallout\/video\/please_stand_by.mp4" type="video\/mp4">Video not supported</video></div>
 `
 document.body.innerHTML += video
